8fb513a6c36b6cb92edba2eaf3e097188343b89b
gkcsd <kaleg2025@gmail.com>
2021-08-07 16:57:51 +0530

    setting up multer and render the index ejs

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const ejs = require('ejs');
-const upload = require('multer');
+const multer = require('multer');
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+//setting multer here..
+var storage = multer.diskStorage({
+    destination: function(req,file,cb) {
+        cb(null, "/public/myupload/");
+    },
+    filename: function(req,file,cb) {
+        cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+    }
+});
+
+var upload = multer({
+    storage: storage,
+}).single("myimage");
+
+//set for ejs
+app.set("view engine", "ejs");
+//set to static folder
+app.use(express.static("./public"));
+
 app.get("/", (req,res) => {
-    res.send("Multer is running fine and so is nodemon..");
+    res.render("index");
 });
 
 app.listen(port, () => console.log(`Server is running at ${port}..`));
